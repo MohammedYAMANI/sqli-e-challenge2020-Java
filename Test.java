5aa69e4c580c2b49383f75854013ee49edfb60e3
MohammedYAMANI <51216153+MohammedYAMANI@users.noreply.github.com>
2019-12-09 16:10:58 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,3 +1,5 @@
+package com.yamed.sqli.echallenge.test;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -13,101 +15,100 @@ import java.util.Set;
 
 public class Test {
 
-	public static void main(String[] args) {
-		List<Integer> ListOfDuplicateNumbers = new ArrayList();
+	public static void main(final String[] args) {
+
+		final List<Integer> ListOfDuplicateNumbers = new ArrayList<>();
+
 		ListOfDuplicateNumbers.add(1);
 		ListOfDuplicateNumbers.add(5);
 		ListOfDuplicateNumbers.add(1);
 		ListOfDuplicateNumbers.add(3);
 		ListOfDuplicateNumbers.add(3);
 		ListOfDuplicateNumbers.add(2);
-		
-		String message = " ymm yamani Mohammed med code";
+
+		final String message = " ymm yamani Mohammed med code";
 		// Test compress
-		System.out.println("compresse msg : " +message);
+		System.out.println("compresse msg : " + message);
 		System.out.println(compress(message));
-		
-		// Test count non unique 
+
+		// Test count non unique
 		System.out.println("Count non unique : ");
 		System.out.println(ListOfDuplicateNumbers);
 		System.out.println(countNonUniuque(ListOfDuplicateNumbers));
-		 
-		 // Test count pair 
+
+		// Test count pair
 		System.out.println("Count pair : ");
 		System.out.println(countPair(ListOfDuplicateNumbers, 2));
 
 	}
-	
+
 	/**
 	 * 
 	 * @param input contains a string
-	 * @return string msg as a compressed message
+	 * @return string message as a compressed message
 	 */
-	public static String compress(String message) {
-		 int length = message.length();
-		 int count=1;
-	     StringBuilder compressedMessage = new StringBuilder(); 
+	public static String compress(final String message) {
+		final int length = message.length();
+		int count = 1;
+		StringBuilder compressedMessage = new StringBuilder();
 
-		   
+		for (int i = 0; i < length; i++) {
 
-		    for(int i=0; i<length; i++){
-		        
-		    	if(i== length - 1){         
-		            compressedMessage.append(message.charAt(i)+""+((count == 1) ? "" : count) );
-		            break;
-		        }
+			if (i == length - 1) {
+				compressedMessage.append(message.charAt(i) + "" + ((count == 1) ? "" : count));
+				break;
+			}
 
-		        if(message.charAt(i)==message.charAt(i+1)){   
-		            count++;
-		        }
-		        else{                           
-		            compressedMessage.append(message.charAt(i)+""+((count == 1) ? "" : count) );
-		            count=1;
-		        }
-		   
-		    }
+			if (message.charAt(i) == message.charAt(i + 1)) {
+				count++;
+			} else {
+				compressedMessage.append(message.charAt(i) + "" + ((count == 1) ? "" : count));
+				count = 1;
+			}
+
+		}
 		return compressedMessage.toString();
 	}
-	
+
 	/**
 	 * 
 	 * @param list of number type Integer
-	 * @param k is difference between pair member of list 
+	 * @param k    is difference between pair member of list
 	 * @return count pair
 	 */
-	public static int countPair(List<Integer> listNumbers, int k ) {
-	       Set<Integer> collectionsNumbers = new HashSet<>();
-	        
-	       for(int i = 0;i<listNumbers.size();i++) {
-	        	for(int j = i+1;j<listNumbers.size();j++) {
-	        		if(listNumbers.get(i)!= null && Math.abs(listNumbers.get(i)-listNumbers.get(j)) == k) {
-	        			collectionsNumbers.add(listNumbers.get(i));
-	        			break;
-	        		}
-	        	}
-	        }
+	public static int countPair(final List<Integer> listNumbers, final int k) {
+		Set<Integer> collectionsNumbers = new HashSet<>();
+
+		for (int i = 0; i < listNumbers.size(); i++) {
+			for (int j = i + 1; j < listNumbers.size(); j++) {
+				if (listNumbers.get(i) != null && Math.abs(listNumbers.get(i) - listNumbers.get(j)) == k) {
+					collectionsNumbers.add(listNumbers.get(i));
+					break;
+				}
+			}
+		}
 		return collectionsNumbers.size();
 	}
-	
+
 	/**
 	 * 
-	 * @param list number of type integer 
+	 * @param list number of type integer
 	 * @return count member non unique in list
 	 */
-	public static int countNonUniuque(List<Integer> listNumbers) {
-	       int countNonUniuque = 0 ;
-	       Set<Integer> collectionsNumbers = new HashSet<>();
-	       int sizeList = listNumbers.size();
-	       for(int i = 0;i<sizeList;i++) {
-	        	for(int j = i+1;j<sizeList;j++) {
-	        		if(listNumbers.get(i).equals(listNumbers.get(j))) {
-	        			collectionsNumbers.add(listNumbers.get(i));
-	        			break;
-	        		}
-	        	}
-	        }
-	       	countNonUniuque = collectionsNumbers.size();
-		return  countNonUniuque;
+	public static int countNonUniuque(final List<Integer> listNumbers) {
+		int countNonUniuque = 0;
+		Set<Integer> collectionsNumbers = new HashSet<>();
+		final int sizeList = listNumbers.size();
+		for (int i = 0; i < sizeList; i++) {
+			for (int j = i + 1; j < sizeList; j++) {
+				if (listNumbers.get(i).equals(listNumbers.get(j))) {
+					collectionsNumbers.add(listNumbers.get(i));
+					break;
+				}
+			}
+		}
+		countNonUniuque = collectionsNumbers.size();
+		return countNonUniuque;
 	}
 
 }
